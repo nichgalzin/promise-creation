50baf3b4a3177fad9a2cf1fd11f9aa1f9f7f67d3
nichgalzin <nichgalzin@gmail.com>
2023-10-02 12:25:51 +0100

    Workshop complete

--- challenge/read-file.js
+++ challenge/read-file.js
@@ -1,7 +1,12 @@
 const fs = require("node:fs");
 
 function readFilePromise(filePath) {
-  //
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
 }
 
 module.exports = { readFilePromise };
--- challenge/wait.js
+++ challenge/wait.js
@@ -1,5 +1,7 @@
 function wait(ms) {
-  //
-}
+  return new Promise((resolve) => {
+    setInterval(resolve, ms);
+  });}
 
 module.exports = { wait };
+``
